5d32ee98a5cb9e9dc31a99bf08a9889c408432c6
Gabriel <gjbarret@asu.edu>
2024-11-22 11:23:47 -0700

    baseline configuration

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import {} from './pages/index'
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
