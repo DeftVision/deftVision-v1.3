731588a7d8181991f19dfa0c40758a28ef68a129
Gabriel <gjbarret@asu.edu>
2024-12-08 17:09:31 -0700

    Add shopper management components and routing
    Implemented new ShopperForm and ShopperData components for handling shopper entries and data display. Integrated these components into the navigation and created necessary routes to enable shopper management functionality within the application.

---
+++ frontend/src/shoppers/ShopperData.js
@@ -0,0 +1,84 @@
+import {Box, Table, TableBody, TableHead, TableContainer, TableCell, TableRow, IconButton, Paper} from '@mui/material'
+import { CheckCircleOutline, DoNotDisturb } from '@mui/icons-material'
+import { useState, useEffect } from 'react'
+
+export default function ShopperData () {
+    const [shoppers, setShoppers] = useState([])
+
+    useEffect(() => {
+        async function getShopperVisits() {
+            try {
+                const response = await fetch('http://localhost:8005/api/shopper/', {
+                    method: 'GET',
+                    headers: { 'Content-Type' : 'application/json'}
+                })
+
+                const _response = await response.json();
+
+                if(response.ok && _response.shoppers) {
+                    setShoppers(_response.shoppers);
+                } else {
+                    console.error('error fetching shopper data')
+                }
+
+            } catch (error) {
+                console.error('failed to get shopper data')
+            }
+        }
+        getShopperVisits();
+    }, []);
+
+    const handlePublishedStatus = async (shopperId, currentStatus) =>  {
+        try {
+            const response = await fetch(`http://localhost:8005/api/shopper/status${shopperId}`, {
+                method: 'PATCH',
+                body: JSON.stringify({ isActive: !currentStatus }),
+                headers: { 'Content-Type': 'application/json' }
+            })
+
+            if(response.ok) {
+                const updatedShoppers = shoppers.map((shopper) =>
+                    shopper._id === shopperId
+                        ? { ...shopper, isPublished: !currentStatus }
+                        : shopper
+                );
+                setShoppers(updatedShoppers);
+            } else {
+                console.error('failed to update shopper status')
+            }
+        } catch (error)  {
+            console.error('error updating shopper status', error)
+        }
+    }
+
+    return (
+        <Box width='100%' sx={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center', marginTop: 4}}>
+            <Paper elevation={16} width='100%' sx={{padding: 5, maxWidth: '1200px', width: '90%'}}>
+                <Box sx={{width: '50%', justifyContent: 'center', margin: 'auto', paddingTop: 5}}>
+                    <TableContainer>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Date</TableCell>
+                                    <TableCell>Location</TableCell>
+                                    <TableCell>Final Score</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {shoppers.map((shopper) => (
+                                    <TableRow key={shopper._id}>
+                                        <TableCell>{shopper.dateTime}</TableCell>
+                                        <TableCell>{shopper.location}</TableCell>
+                                        <TableCell>{shopper.finalScore}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Box>
+            </Paper>
+
+        </Box>
+    );
+};
+
---
+++ frontend/src/shoppers/ShopperForm.js
@@ -0,0 +1,262 @@
+import {
+    Box,
+    Button,
+    Paper,
+    Stack,
+    TextField,
+    MenuItem,
+    Select,
+    Switch,
+    FormControl,
+    FormControlLabel,
+    InputLabel
+} from "@mui/material";
+import { useState } from "react";
+
+import otherLocations from "../utilities/OtherLocations";
+
+const form_fields = {
+    dateTime: Date.now(),
+    shopperName: '',
+    location: '',
+    greeting: false,
+    cashier: '',
+    orderRepeated: '',
+    upsell: false,
+    wait: '',
+    foodScore: '',
+    serviceScore: '',
+    cleanScore: '',
+    finalScore: '',
+    comments: ''
+}
+
+export default function ShopperForm () {
+    const [formData, setFormData] = useState(form_fields);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch('http://localhost:8005/api/shopper', {
+                method: 'POST',
+                body: JSON.stringify(formData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            const _response = await response.json()
+            if(response.ok && _response.shopper) {
+                setFormData(formData)
+                console.log(_response.message)
+            } else {
+                console.error('invalid response structure', _response.error)
+            }
+        } catch (error) {
+            console.log('failed to submit',error)
+        }
+    }
+
+    return (
+        <Box width='100%' sx={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center', marginTop: 4}}>
+            <Paper elevation={8} width='100%' sx={{padding: 5, maxWidth: '1200px', width: '90%'}}>
+                <Box sx={{width: '50%', justifyContent: 'center', margin: 'auto', paddingTop: 5}}>
+                    <form onSubmit={handleSubmit}>
+                        <Stack direction='column' spacing={3}>
+                            <TextField
+                                type='datetime-local'
+                                label='Visit Date'
+                                value={formData.dateTime}
+                                onChange={(e) => {
+                                    setFormData({
+                                        ...formData,
+                                        dateTime: e.target.value
+                                    })
+                                }}
+                                sx={{width: '500px'}}
+                            />
+                            <TextField
+                                type='text'
+                                label='Shopper Name'
+                                value={formData.shopperName}
+                                onChange={(e) => {
+                                    setFormData({
+                                        ...formData,
+                                        shopperName: e.target.value
+                                    })
+                                }}
+                                sx={{width: '500px'}}
+                            />
+                            <FormControl>
+                                <InputLabel>Location</InputLabel>
+                                <Select
+                                    label='Location'
+                                    variant='outlined'
+                                    value={formData.location || '' }
+                                    onChange={(e) => {
+                                        setFormData({
+                                            ...formData,
+                                            location: e.target.value
+                                        })
+                                    }}
+                                    sx={{width: '500px'}}
+
+                                >
+                                    {otherLocations.map((location) => (
+                                        <MenuItem key={location} value={location}>
+                                            {location}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                            <TextField
+                                type='text'
+                                label='Cashier Name '
+                                value={formData.cashier}
+                                onChange={(e) => {
+                                    setFormData({
+                                        ...formData,
+                                        cashier: e.target.value
+                                    })
+                                }}
+                                sx={{width: '500px'}}
+                            />
+                            <FormControlLabel
+                                control={
+                                    <Switch
+                                        name='greeting'
+                                        checked={formData.greeting}
+                                        onChange={(e) => {
+                                            setFormData({
+                                                ...formData,
+                                                greeting: e.target.checked
+                                            })
+                                        }}
+                                    />
+                                }
+                                label='Warm friendly greeting'
+                            >
+                            </FormControlLabel>
+
+                            <FormControlLabel
+                                control={
+                                    <Switch
+                                        name='upsell'
+                                        checked={formData.upsell}
+                                        onChange={(e) => {
+                                            setFormData({
+                                                ...formData,
+                                                upsell: e.target.checked
+                                            })
+                                        }}
+                                    />
+                                }
+                                label='Upsell sweet potato fries'
+                            >
+                            </FormControlLabel>
+                            <FormControlLabel
+                                control={
+                                    <Switch
+                                        name='greeting'
+                                        checked={formData.orderRepeated}
+                                        onChange={(e) => {
+                                            setFormData({
+                                                ...formData,
+                                                orderRepeated: e.target.checked
+                                            })
+                                        }}
+                                    />
+                                }
+                                label='Cashier repeated the order back to you'
+                            >
+                            </FormControlLabel>
+                            <TextField
+                                type='number'
+                                label='Wait time [minutes]'
+                                value={formData.wait}
+                                onChange={(e) => {
+                                    setFormData({
+                                        ...formData,
+                                        wait: e.target.value
+                                    })
+                                }}
+                                sx={{width: '500px'}}
+                            />
+
+                            <TextField
+                                type='number'
+                                label='Food score [1-10]'
+                                value={formData.foodScore}
+                                onChange={(e) => {
+                                    setFormData({
+                                        ...formData,
+                                        foodScore: e.target.value
+                                    })
+                                }}
+                                sx={{width: '500px'}}
+                            />
+
+                            <TextField
+                                type='number'
+                                label='Service score [1-10]'
+                                value={formData.serviceScore}
+                                onChange={(e) => {
+                                    setFormData({
+                                        ...formData,
+                                        serviceScore: e.target.value
+                                    })
+                                }}
+                                sx={{width: '500px'}}
+                            />
+
+                            <TextField
+                                type='number'
+                                label='Clean score [1-10]'
+                                value={formData.cleanScore}
+                                onChange={(e) => {
+                                    setFormData({
+                                        ...formData,
+                                        cleanScore: e.target.value
+                                    })
+                                }}
+                                sx={{width: '500px'}}
+                            />
+
+                            <TextField
+                                type='number'
+                                label='Final score [1-10]'
+                                value={formData.finalScore}
+                                onChange={(e) => {
+                                    setFormData({
+                                        ...formData,
+                                        finalScore: e.target.value
+                                    })
+                                }}
+                                sx={{width: '500px'}}
+                            />
+                            <TextField
+                                type='text'
+                                label='Comments'
+                                value={formData.comments}
+                                onChange={(e) => {
+                                    setFormData({
+                                        ...formData,
+                                        comments: e.target.value
+                                    })
+                                }}
+                                multiline
+                                rows={5}
+                                sx={{width: '500px'}}
+                            />
+
+
+
+
+                            <Button type='submit' variant='outlined'>save</Button>
+                        </Stack>
+                    </form>
+                </Box>
+            </Paper>
+        </Box>
+    );
+};
+
