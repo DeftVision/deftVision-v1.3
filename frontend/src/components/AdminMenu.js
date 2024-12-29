a7cc774e776a4e6951a9ca37f532af38164d20ee
Gabriel <gjbarret@asu.edu>
2024-12-29 11:11:39 -0700

    Refactor UI components with reusable CardTemplate
    Replaced repeated card rendering logic in AdminMenu and ViewableAnnouncements with a new reusable CardTemplate component. Simplified and streamlined the code for better maintainability and consistency. Introduced minor adjustments to loading and filtering logic in ViewableAnnouncements to enhance clarity.

--- frontend/src/components/AdminMenu.js
+++ frontend/src/components/AdminMenu.js
@@ -1,17 +1,35 @@
-import {
-    Box,
-    ButtonBase,
-    Card,
-    CardContent,
-    Typography,
-} from '@mui/material';
-import { Article, Campaign, PeopleAlt, Grading, DynamicForm } from '@mui/icons-material';
-import { Link } from 'react-router-dom';
-import { useTheme } from '@mui/material/styles';
-import React from 'react'
+import { Article, Campaign, DynamicForm, Grading, PeopleAlt } from '@mui/icons-material';
+import { Avatar, Box } from "@mui/material";
+import CardTemplate from './CardTemplate'
+
 export default function AdminMenu() {
-    const theme = useTheme();
-    const isDarkMode = theme.palette.mode === 'dark';
+    const menuItems = [
+        {
+            title: 'Announcements',
+            icon: <Campaign/>,
+            link: '/manage-announcements',
+        },
+        {
+            title: 'Documents',
+            icon: <Article/>,
+            link: '/manage-documents',
+        },
+        {
+            title: 'Users',
+            icon: <PeopleAlt/>,
+            link: '/users',
+        },
+        {
+            title: 'Shopper Visit',
+            icon: <Grading/>,
+            link: '/shoppers',
+        },
+        {
+            title: 'Form Templates',
+            icon: <DynamicForm/>,
+            link: '#',
+        },
+    ];
 
     return (
         <Box
@@ -23,87 +41,24 @@ export default function AdminMenu() {
                 padding: 2,
             }}
         >
-            {[
-                {
-                    title: 'Announcements',
-                    icon: <Campaign />,
-                    link: '/manage-announcements',
-                },
-                {
-                    title: 'Documents',
-                    icon: <Article />,
-                    link: '/manage-documents',
-                },
-                {
-                    title: 'Users',
-                    icon: <PeopleAlt />,
-                    link: '/users',
-                },
-                {
-                    title: 'Shopper Visit',
-                    icon: <Grading />,
-                    link: '/shoppers',
-                },
-                {
-                    title: 'Form Templates',
-                    icon: <DynamicForm />,
-                    link: '#',
-                },
-            ].map(({ title, icon, link }) => (
-                <ButtonBase
+            {menuItems.map(({title, icon, link} ) => (
+                <CardTemplate
                     key={title}
-                    component={Link}
-                    to={link}
-                    sx={{
-                        width: { xs: '100%', sm: '250px' }, 
-                        height: '150px',
-                        textAlign: 'center',
-                        borderRadius: '8px',
-                    }}
-                >
-                    <Card
-                        sx={{
-                            width: '100%',
-                            height: '100%',
-                            position: 'relative',
-                            overflow: 'hidden',
-                            boxShadow: 3,
-                            transition: 'transform 0.3s, box-shadow 0.3s, background-color 0.3s',
-                            backgroundColor: isDarkMode
-                                ? 'rgba(255, 255, 255, 0.05)' // Subtle white tint for dark mode
-                                : 'rgba(0, 0, 0, 0.05)', // Subtle black tint for light mode
-                            '&:hover': {
-                                transform: 'scale(1.05)',
-                                boxShadow: 6,
-                                backgroundColor: isDarkMode
-                                    ? 'rgba(255, 255, 255, 0.2)' // More white tint for dark mode
-                                    : 'rgba(0, 0, 0, 0.2)', // More black tint for light mode
-                            },
-                            color: isDarkMode ? 'white' : 'black',
-                        }}
-                    >
-                        {React.cloneElement(icon, {
-                            sx: {
-                                position: 'absolute',
-                                fontSize: '8rem',
-                                color: isDarkMode
-                                    ? 'rgba(255, 255, 255, 0.1)'
-                                    : 'rgba(0, 0, 0, 0.1)',
-                                top: '50%',
-                                left: '50%',
-                                transform: 'translate(-50%, -50%)',
-                                zIndex: 1,
-                                transition: 'transform 0.3s',
-                                '&:hover': {
-                                    transform: 'translate(-50%, -50%) scale(1.1)',
-                                },
-                            },
-                        })}
-                        <CardContent sx={{ zIndex: 2 }}>
-                            <Typography variant="h6">{title}</Typography>
-                        </CardContent>
-                    </Card>
-                </ButtonBase>
+                    title={title}
+                    avatar={
+                        <Avatar
+                            sx={{
+                                backgroundColor: 'primary.main',
+                                width: 40,
+                                height: 40,
+                            }}
+                        >
+                            { icon }
+                        </Avatar>
+                    }
+                    hoverEffect
+                    onClick={() => {window.location.href = link}}
+                />
             ))}
         </Box>
     );
---
+++ frontend/src/components/CardTemplate.js
@@ -0,0 +1,70 @@
+import { Box, Card, CardHeader, CardContent, Avatar, Typography } from '@mui/material';
+
+
+export default function CardTemplate({
+    title,
+    subtitle,
+    avatar,
+    icon,
+    content,
+    onClick,
+    hoverEffect = true,
+}) {
+    return (
+        <Card
+            sx={{
+                width: { xs: '100%', sm: 300 },
+                height: 'auto',
+                position: 'relative',
+                borderRadius: 2,
+                boxShadow: 3,
+                overflow: 'hidden',
+                transition: hoverEffect ? 'transform 0.3s, box-shadow: 0.3s' : undefined,
+                '&:hover': hoverEffect ? {
+                    transform: 'scale(1.05)',
+                    boxShadow: 6,
+                }
+                : undefined,
+                cursor: onClick ? 'pointer' : 'default',
+            }}
+            onClick={onClick}
+        >
+            <CardHeader
+                avatar={avatar}
+                title={
+                    <Typography
+                        variant='h6'
+                        sx={{
+                            position: 'relative',
+                            zIndex: 2,
+                        }}
+                    >
+                        {title}
+                    </Typography>
+                }
+                subheader={
+                    subtitle && (
+                        <Typography
+                            variant='body2'
+                            color='textSecondary'
+                            sx={{
+                                position: 'relative',
+                                zIndex: 2,
+                            }}
+                            >
+                            {subtitle}
+                        </Typography>
+                    )
+                }
+                subHeader={
+                subtitle && (
+                    <Typography variant='body2' color='textSecondary'>
+                        {subtitle}
+                    </Typography>
+                    )
+                }
+            />
+            <CardContent sx={{ zIndex: icon ? 2 : undefined }}>{content}</CardContent>
+        </Card>
+    );
+}
