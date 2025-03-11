80285d214506bc93d53efbb8728e352c0c53f3b7
Gabriel <gjbarret@asu.edu>
2025-03-10 23:46:15 -0600

    removed the port number from server.js

--- backend/server.js
+++ backend/server.js
@@ -78,7 +78,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 const HOST = process.env.HOST || "0.0.0.0";
 
 app.listen(PORT, HOST, () => {
